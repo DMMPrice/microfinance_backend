9c80608aaf37dcbb6a19ddd8b361ffd7191f1760
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2025-12-17 09:29:42 +0530

    Version 0.1 -> IV

--- main.py
+++ main.py
@@ -43,7 +43,10 @@ app = FastAPI(title="Microfinance Backend API", version="1.0")
 # --------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Restrict for production
+    allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
